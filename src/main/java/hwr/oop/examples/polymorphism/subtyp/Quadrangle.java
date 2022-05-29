87062eba6eefd8acca13f5cb0fe5c802150f4e3c
Nicolas Kohl <nicolas.kohl@sap.com>
2022-05-29 16:41:19 +0200

    Shapes finish

--- src/main/java/hwr/oop/examples/polymorphism/subtyp/Quadrangle.java
+++ src/main/java/hwr/oop/examples/polymorphism/subtyp/Quadrangle.java
@@ -8,6 +8,8 @@ public interface Quadrangle extends Shape, Comparable<Quadrangle> {
 
     @Override
     default int compareTo(Quadrangle o) {
-        return Double.compare(getAreaSize(), o.getAreaSize());
+        return Double.compare(getAreaSize(), o.getAreaSize()) +
+                Double.compare(getPerimeterLength(), o.getPerimeterLength()) +
+                Double.compare(getNumberOfSides() * 90.0, o.getNumberOfSides() * 90.0);
     }
 }
---
+++ src/main/java/hwr/oop/examples/polymorphism/subtyp/ShapeFactory.java
@@ -0,0 +1,4 @@
+package hwr.oop.examples.polymorphism.subtyp;
+
+public class ShapeFactory {
+}
